c39ee7f722710989362a74440811adc159689813
editor-5 <159111430+editor-5@users.noreply.github.com>
2024-02-07 18:27:18 +0900

    add say_hello funtion

--- calculator.py
+++ calculator.py
@@ -8,21 +8,11 @@ def subtract(a, b):
 def multiply(a, b):
     return a*b
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-def multiply(a, b):
-    return a*b
-
-def devide(a, b):
-    return a/b    
-
-=======
-def divide(a, b):
-=======
 def divide_premium(a, b):
->>>>>>> master
     return a/b    
 
+def say_hello()
+    print("hello")
     # ex001
     # ex002
->>>>>>> master
+ 
